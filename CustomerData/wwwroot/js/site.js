5e6208012bd83b80c3534f82e4d14b2c59ebb57b
Wilson Neto <contato@wilsonneto.com.br>
2019-07-04 22:10:56 -0300

    Adicionar arquivos de projeto.

---
+++ CustomerData/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿app = {
+
+    // inicialização e configuração
+    config: function () {
+        var body = document.getElementsByTagName("body")[0];
+        body.addEventListener( "load", this.api_send(), false );
+    },
+
+    // pegando os dados
+    get_data: function () {
+
+        var pack = new Object();
+
+        pack.params = window.location.search;
+        pack.page_title = document.title;
+        pack.browser = navigator.vendor + navigator.appCodeName + navigator.appName + navigator.userAgent;
+
+        return json = JSON.stringify(pack);
+    },
+
+    // enviando para a api
+    api_send: function () {
+        this.get_data();
+        window.alert("thats ok!");
+    }
+
+}
+
+app.config();
